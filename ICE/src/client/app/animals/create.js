9dd10b6e250eac2ab8ee54a84cd636f2b2a17529
CheyenneNors <156718436+CheyenneNors@users.noreply.github.com>
2025-01-10 20:13:27 -0500

    created some js files and added some comment headers.

---
+++ ICE/src/client/app/animals/create.js
@@ -0,0 +1,7 @@
+/*
+Name: Cheyenne Norsworthy
+Filename: create.js
+Course: INFT 2202
+Date: January 10, 2025
+Description: This is my general js create page.
+*/
---
+++ ICE/src/client/app/animals/service.js
@@ -0,0 +1,7 @@
+/*
+Name: Cheyenne Norsworthy
+Filename: service.js
+Course: INFT 2202
+Date: January 10, 2025
+Description: This is my general js service page.
+*/
