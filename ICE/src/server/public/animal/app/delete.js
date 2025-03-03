f8406776e5d66a95b6403dbbf6b4d5405cecb698
CheyenneNors <156718436+CheyenneNors@users.noreply.github.com>
2025-03-03 08:56:34 -0500

    updated server folder

---
+++ ICE/src/server/public/animal/app/delete.js
@@ -0,0 +1,7 @@
+/*
+Name: Cheyenne Norsworthy
+Filename: delete.js
+Course: INFT 2202
+Date: January 10, 2025
+Description: This is my general delete js page.
+*/
---
+++ ICE/src/server/public/animal/app/update.js
@@ -0,0 +1,7 @@
+/*
+Name: Cheyenne Norsworthy
+Filename: update.js
+Course: INFT 2202
+Date: January 10, 2025
+Description: This is my general update js page.
+*/
