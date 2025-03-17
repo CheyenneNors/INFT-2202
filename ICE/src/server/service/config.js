167b6e6c6d0f00e8b3bf9b0ac1f937c368976250
CheyenneNors <156718436+CheyenneNors@users.noreply.github.com>
2025-03-17 09:19:33 -0400

    added validation code

--- ICE/src/server/service/config.js
+++ ICE/src/server/service/config.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import animalRouter from '../routes/animal.js'
 import { loggingMiddleware } from '../middleWare/logging.js';
+import { query, validationResult } from 'express-validator';
 
 function config(app) {
     
@@ -11,8 +12,12 @@ function config(app) {
     app.use(loggingMiddleware);
 
     let helloContent = `<!DOCTYPE html><html lang=\"en-us\"><head><title>INFT 2202</title></head><body><main><h1>Hello from Express</h1><p>at ${new Date()}</p></main></body></html>`;
-    app.get("/hello", (req, res) => {
-        res.send(helloContent);
+    app.get("/hello", query('person').notEmpty(), (req, res) => {
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+            res.send(`Hello, ${req.query.person}!`);
+        }
+        res.status(304).json('missing person');
     }); 
 
     app.use('/api/animals', animalRouter);
